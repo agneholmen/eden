4b6c6c8a53dfe477a4c9af1c342074589ff1ba89
Emil Agneholm <emil.agneholm@gmail.com>
2024-11-22 21:18:44 +0100

    Rabbits can now move to find food. Rabbits can now breed. Tweaked some numbers. Input parameters for height, width, and iterations.

--- animals.py
+++ animals.py
@@ -1,11 +1,16 @@
 import enums
 import random
+import string
 
 STAT_BASE = 20
 WOLF_BASE_STRENGTH = 40
 WOLF_BASE_AGILITY = 20
 RABBIT_BASE_STRENGTH = 20
 RABBIT_BASE_AGILITY = 30
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 class Animal:
     def __init__(self,  strength=STAT_BASE, agility=STAT_BASE):
@@ -15,6 +20,15 @@ class Animal:
         self.strength = strength
         self.agility = agility
         self.fatigue = 0
+        self.breeding = 5
+        self.name = id_generator()
+
+    def is_dead(self):
+        return True if self.energy <= 0 else False
+    
+    def increase_age(self, amount=1):
+        self.age += amount
+        self.breeding = max(0, self.breeding - amount)
 
     def __str__(self):
         return "Energy: {0}\nSex: {1}\nAge: {2}\nStrength: {3}\nAgility: {4}\nFatigue: {5}".format(self.energy, self.sex, self.age, self.strength, self.agility, self.fatigue)
@@ -22,6 +36,15 @@ class Animal:
 class Rabbit(Animal):
     def __init__(self, strength=RABBIT_BASE_STRENGTH, agility=RABBIT_BASE_AGILITY):
         super().__init__(strength, agility)
+
+    def eat_grass(self, grass, amount=10):
+        self.energy += grass.eaten(amount)
+
+    def tire(self, amount=5):
+        self.energy -= amount
+
+    def __str__(self):
+        return f"Name: {self.name}, Sex: {self.sex}, Strength: {self.strength}, Agility: {self.agility}, Energy: {self.energy}"
 
 class Wolf(Animal):
     def __init__(self, strength=WOLF_BASE_STRENGTH, agility=WOLF_BASE_AGILITY):
--- simulate.py
+++ simulate.py
@@ -1,120 +1,227 @@
-#!/usr/bin/python3
-
 from colorama import Fore, Style, Back
-from time import sleep
 
 import numpy as np
+
+import argparse
+import time
 
 from animals import Wolf
 from animals import Rabbit
 import random
 
-MAX_HEIGHT = 5
-MAX_WIDTH = 5
-
 class Area:
     def __init__(self):
-        self.grass = Grass() if random.randint(1, 4) == 1 else None
-        self.rabbits = [Rabbit()]
-        self.wolves = [Wolf()]
+        self.grass = Grass() if random.randint(1, 3) == 1 else None
+        self.rabbits = [Rabbit()] if random.randint(1, 2) == 1 else []
+        self.wolves = [Wolf()] if random.randint(1, 2) == 1 else []
 
 class Grass:
-    max_size = 20
+    max_size = 50
 
     def __init__(self, size=10):
         self.size = size
 
     def grow(self):
-        self.size += 1
+        self.size += 20
         if self.size > Grass.max_size:
             self.size = Grass.max_size
 
-def print_world(world):
-    print("--------------------------")
-    for row in world:
-        # Grass
-        row_string = "|"
-        for cell in row:
-            if cell.grass:
-                if cell.grass.size >= 10:
-                    row_string += f" {Fore.GREEN}{Style.BRIGHT}{str(cell.grass.size)}{Style.RESET_ALL} |"
-                else:
-                    row_string += f" {Fore.GREEN}{Style.BRIGHT}{str(cell.grass.size)}{Style.RESET_ALL}  |"
-            else:
-                row_string += "    |"
-        print(row_string)
+    def eaten(self, amount):
+        self.size -= amount
+        return amount if self.size >= 0 else amount + self.size
 
-        # Rabbits
-        row_string = "|"
-        for cell in row:
-            if cell.rabbits:
-                if len(cell.rabbits) >= 10:
-                    row_string += f" {Fore.BLUE}{Style.BRIGHT}{str(len(cell.rabbits))}{Style.RESET_ALL} |"
-                else:
-                    row_string += f" {Fore.BLUE}{Style.BRIGHT}{str(len(cell.rabbits))}{Style.RESET_ALL}  |"
-            else:
-                row_string += "    |"
-        print(row_string)
+    def exists(self):
+        return True if self.size > 0 else False
 
-        # Wolves
-        row_string = "|"
-        for cell in row:
-            if cell.wolves:
-                if len(cell.wolves) >= 10:
-                    row_string += f" {Fore.RED}{Style.BRIGHT}{str(len(cell.wolves))}{Style.RESET_ALL} |"
-                else:
-                    row_string += f" {Fore.RED}{Style.BRIGHT}{str(len(cell.wolves))}{Style.RESET_ALL}  |"
-            else:
-                row_string += "    |"
-        print(row_string)
+class World:
+    def __init__(self, iterations, max_width, max_height):
+        self.iterations = iterations
+        self.max_width = max_width
+        self.max_height = max_height
 
+        self.map = []
+        for _ in range(0, self.max_height):
+            row = []
+            for _ in range(0, self.max_width):
+                row.append(Area())
+            self.map.append(row)
+
+    def print_world(self):
         print("--------------------------")
+        for row in self.map:
+            # Grass
+            row_string = "|"
+            for cell in row:
+                if cell.grass:
+                    if cell.grass.size >= 10:
+                        row_string += f" {Fore.GREEN}{Style.BRIGHT}{str(cell.grass.size)}{Style.RESET_ALL} |"
+                    else:
+                        row_string += f" {Fore.GREEN}{Style.BRIGHT}{str(cell.grass.size)}{Style.RESET_ALL}  |"
+                else:
+                    row_string += "    |"
+            print(row_string)
 
-def find_neighbours(row_index, cell_index):
-    neighbours = []
+            # Rabbits
+            row_string = "|"
+            for cell in row:
+                if cell.rabbits:
+                    if len(cell.rabbits) >= 10:
+                        row_string += f" {Fore.BLUE}{Style.BRIGHT}{str(len(cell.rabbits))}{Style.RESET_ALL} |"
+                    else:
+                        row_string += f" {Fore.BLUE}{Style.BRIGHT}{str(len(cell.rabbits))}{Style.RESET_ALL}  |"
+                else:
+                    row_string += "    |"
+            print(row_string)
 
-    possible = [(row_index - 1, cell_index - 1), (row_index - 1, cell_index),
-                (row_index - 1, cell_index + 1), (row_index, cell_index - 1),
-                (row_index, cell_index + 1), (row_index + 1, cell_index - 1),
-                (row_index + 1, cell_index), (row_index + 1, cell_index + 1)]
+            # Wolves
+            row_string = "|"
+            for cell in row:
+                if cell.wolves:
+                    if len(cell.wolves) >= 10:
+                        row_string += f" {Fore.RED}{Style.BRIGHT}{str(len(cell.wolves))}{Style.RESET_ALL} |"
+                    else:
+                        row_string += f" {Fore.RED}{Style.BRIGHT}{str(len(cell.wolves))}{Style.RESET_ALL}  |"
+                else:
+                    row_string += "    |"
+            print(row_string)
 
-    for p in possible:
-        if 0 <= p[0] <= MAX_WIDTH - 1 and 0 <= p[1] <= MAX_HEIGHT - 1:
-            neighbours.append(p)
+            print("--------------------------")
+            
+        print(f"Number of rabbits: {str(self.get_number_of_rabbits())}")
+        print(f"Number of grass: {str(self.get_number_of_grass())}")
 
-    return neighbours
+    def find_neighbours(self, row_index, cell_index):
+        neighbours = []
 
-def main():
-    world = np.array([
-                      [Area(), Area(), Area(), Area(), Area()],
-                      [Area(), Area(), Area(), Area(), Area()],
-                      [Area(), Area(), Area(), Area(), Area()],
-                      [Area(), Area(), Area(), Area(), Area()],
-                      [Area(), Area(), Area(), Area(), Area()]
-                     ])
+        possible = [(row_index - 1, cell_index - 1), (row_index - 1, cell_index),
+                    (row_index - 1, cell_index + 1), (row_index, cell_index - 1),
+                    (row_index, cell_index + 1), (row_index + 1, cell_index - 1),
+                    (row_index + 1, cell_index), (row_index + 1, cell_index + 1)]
+
+        for p in possible:
+            if 0 <= p[0] <= self.max_width - 1 and 0 <= p[1] <= self.max_height - 1:
+                neighbours.append(p)
+
+        return neighbours
+
+    def get_random_neighbour(self, row_index, cell_index):
+        neighbours = self.find_neighbours(row_index, cell_index)
+        r = random.randint(0, len(neighbours) - 1)
+        neighbour = neighbours[r]
+        
+        return neighbour
+    
+    def get_number_of_rabbits(self):
+        rabbits = 0
+
+        for row in self.map:
+            for cell in row:
+                if cell.rabbits:
+                    rabbits += len(cell.rabbits)
+
+        return rabbits
+    
+    def get_number_of_grass(self):
+        grass = 0
+
+        for row in self.map:
+            for cell in row:
+                if cell.grass:
+                    grass += 1
+
+        return grass
+
+def main(iterations, max_height, max_width):
+    world = World(iterations, max_height, max_width)
 
     print("Original:")
-    print_world(world)
+    world.print_world()
 
-    for i in range(1, 11):
+    for i in range(1, iterations + 1):
         print(f"Iteration {str(i)}:")
         # Grow
-        for row_index, row in enumerate(world):
+        for row_index, row in enumerate(world.map):
             for cell_index, cell in enumerate(row):
                 if cell.grass:
                     cell.grass.grow()
 
         # Spread seeds
-        for row_index, row in enumerate(world):
+        for row_index, row in enumerate(world.map):
             for cell_index, cell in enumerate(row):
                 if cell.grass:
-                    neighbours = find_neighbours(row_index, cell_index)
-                    r = random.randint(0, len(neighbours) - 1)
-                    s = neighbours[r]
-                    if not world[s[0]][s[1]].grass:
-                        world[s[0]][s[1]].grass = Grass(1)
+                    s = world.get_random_neighbour(row_index, cell_index)
+                    if not world.map[s[0]][s[1]].grass:
+                        world.map[s[0]][s[1]].grass = Grass(1)
 
-        print_world(world)
+        # Rabbits
+        for row_index, row in enumerate(world.map):
+            for cell_index, cell in enumerate(row):
+                if cell.rabbits:
+                    for rabbit in cell.rabbits:
+                        # Lose energy
+                        rabbit.tire()
+                        # Age
+                        rabbit.increase_age()
+                        # First eat if there's any grass and energy is 40 or below
+                        if cell.grass and rabbit.energy <= 40:
+                            rabbit.eat_grass(cell.grass)
+                            if not cell.grass.exists():
+                                cell.grass = None
+                        # Otherwise move to different cell or die
+                        elif rabbit.energy < 40:
+                            if rabbit.is_dead():
+                                cell.rabbits.remove(rabbit)
+                            else:
+                                neighbours = world.find_neighbours(row_index, cell_index)
+                                for n in neighbours:
+                                    if world.map[n[0]][n[1]].grass:
+                                        world.map[n[0]][n[1]].rabbits.append(rabbit)
+                                        cell.rabbits.remove(rabbit)
+                                        break
+                                else:
+                                    s = world.get_random_neighbour(row_index, cell_index)
+                                    world.map[s[0]][s[1]].rabbits.append(rabbit)
+                                    cell.rabbits.remove(rabbit)
+                            continue
+
+                        # Attempt to breed. First find partner.
+                        if rabbit.breeding == 0 and rabbit.energy > 40:
+                            # No other rabbits. Need to move
+                            if len(cell.rabbits) == 1:
+                                neighbours = world.find_neighbours(row_index, cell_index)
+                                for n in neighbours:
+                                    if world.map[n[0]][n[1]].rabbits:
+                                        world.map[n[0]][n[1]].rabbits.append(rabbit)
+                                        cell.rabbits.remove(rabbit)
+                                        break
+                                else:
+                                    s = world.get_random_neighbour(row_index, cell_index)
+                                    world.map[s[0]][s[1]].rabbits.append(rabbit)
+                                    cell.rabbits.remove(rabbit)
+                            else:
+                                for partner in cell.rabbits:
+                                    if partner != rabbit and partner.sex != rabbit.sex and partner.breeding == 0:
+                                        partner.breeding = 5
+                                        rabbit.breeding = 5
+                                        r = Rabbit()
+                                        r.strength = int(((partner.strength + rabbit.strength) / 2) + random.randint(0, 5))
+                                        r.agility = int(((partner.agility + rabbit.agility) / 2) + random.randint(0, 5))
+
+                                        cell.rabbits.append(r)
+                                        break
+
+                        # Check if dead and remove
+                        if rabbit.is_dead():
+                            cell.rabbits.remove(rabbit)
+
+        world.print_world()
+        time.sleep(3)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--iterations", help="Number of iterations.", type=int, default=10)
+    parser.add_argument("-mh", "--height", help="The height of the map in number of cells.", type=int, default=5)
+    parser.add_argument("-mw", "--width", help="The width of the map in number of cells.", type=int, default=5)
+    args = parser.parse_args()
+
+    main(args.iterations, args.height, args.width)
